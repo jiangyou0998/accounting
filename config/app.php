67ef73881d89760f951c7bb6ef4f1b1c10da52d5
jianl <fs378354476@outlook.com>
2020-12-07 09:37:51 +0000

    極驗驗證碼測試

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Germey\Geetest\GeetestServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Geetest' => Germey\Geetest\Geetest::class,
 
     ],
 
