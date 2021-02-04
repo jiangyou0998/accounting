67385c78f6bea7eaa0b060b4e45e8da671cc79eb
jianl <fs378354476@outlook.com>
2021-02-04 02:41:07 +0000

    導航欄需要登錄

--- resources/views/layouts/_nav_item.blade.php
+++ resources/views/layouts/_nav_item.blade.php
@@ -1,3 +1,15 @@
-<li class="nav-item">
-    <a class="nav-link" href="{{$menu->uri}}">{{$menu->title}}</a>
+<li class="nav-item dropdown">
+
+    @if($menu->allChildrenMenu->count() && Auth::check())
+        <a class="nav-link" data-toggle="dropdown" href="{{$menu->uri}}">{{$menu->title}}</a>
+        <ul class="dropdown-menu">
+            @foreach($menu->allChildrenMenu as $item)
+                <li class="list-group-item"><a href="{{$item->uri}}">{{$item->title}}</a></li>
+            @endforeach
+        </ul>
+    @elseif($menu->allChildrenMenu->count())
+        <a class="nav-link" href="{{route('login')}}">{{$menu->title}}</a>
+    @else
+        <a class="nav-link" href="{{$menu->uri}}">{{$menu->title}}</a>
+    @endif
 </li>
