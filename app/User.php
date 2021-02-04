522f3b30193727896064cb46d1ad3e094986d56d
jianl <fs378354476@outlook.com>
2021-02-04 02:21:26 +0000

    前台用戶新增分店提交地址失敗

--- app/User.php
+++ app/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
 
     public function address()
     {
-        return $this->hasOne(ShopAddress::class,'id','address_id');
+        return $this->belongsTo(ShopAddress::class,'address_id','id');
     }
 
     public function roles(): BelongsToMany
