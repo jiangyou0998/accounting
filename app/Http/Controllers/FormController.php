0398e19bbc4482bac3050c5312398d8bcb451b28
jianl <fs378354476@outlook.com>
2021-02-09 03:06:40 +0000

    通告跳轉分頁錯誤 分店修改訂單時更新order_date

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,22 +11,15 @@ class FormController extends Controller
     //
     public function index(Request $request)
     {
-        $formModel = new Form();
         $dept = $request->dept;
         $search = $request->search;
 
         $forms = Form::getForms($dept , $search);
-//        $notices = $noticeModel
-//            ->where('expired_date','>',now())
-//            ->orderByDesc('modify_date')
-//            ->get();
-
-//        foreach ()
 
         $dept_names = Form::getDeptName();
 
 //        dump($forms->toArray());
 //        dump($dept_names->toArray());
-        return view('forms.index',compact('forms','dept_names'));
+        return view('forms.index',compact('forms','dept_names' , 'search'));
     }
 }
