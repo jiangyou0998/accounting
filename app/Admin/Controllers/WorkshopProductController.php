5e5e4b847b607eff2852cf50141cf3643484c12b
jianl <fs378354476@outlook.com>
2021-04-23 03:10:01 +0000

    截單日期加到7日

--- app/Admin/Controllers/WorkshopProductController.php
+++ app/Admin/Controllers/WorkshopProductController.php
@@ -234,7 +234,15 @@ class WorkshopProductController extends AdminController
                     ->rules(['regex:/([0-1]{1}[0-9]{1}|[2][0-3])[0-5]{1}[0-9]{1}/','size:4'])
                     ->required();
                 $form->radio('phase','截單日期')
-                    ->options([1 => '1日後', 2 => '2日後', 3 => '3日後', -1 => '後勤落單'])
+                    ->options([1 => '1日後',
+                        2 => '2日後',
+                        3 => '3日後',
+                        4 => '4日後',
+                        5 => '5日後',
+                        6 => '6日後',
+                        7 => '7日後',
+                        -1 => '後勤落單'
+                    ])
                     ->required();
                 $form->checkbox('canordertime','出貨期')
                     ->options($week);
