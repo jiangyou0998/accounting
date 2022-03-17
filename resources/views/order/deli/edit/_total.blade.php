87c0ce4a8b5d8eba9f2b03478218d6a21a64e40e
jianl <fs378354476@outlook.com>
2022-03-17 03:11:01 +0000

    收貨頁面下方增加造型包分類總計

--- resources/views/order/deli/edit/_total.blade.php
+++ resources/views/order/deli/edit/_total.blade.php
@@ -2,7 +2,7 @@
         <table class="table1" border="1" cellspacing="0" cellpadding="0"
                style="width:995px; margin:auto; margin-left:1%;">
             <tr>
-                <td width="575px" align="left" bgcolor="#CCFFFF">&nbsp;&nbsp;
+                <td width="969px" align="left" bgcolor="#CCFFFF">&nbsp;&nbsp;
                     <span style="font-size:24px; font-weight:bold;">總數: $
 					<span id="all_total">{{number_format($total_price, 1, '.', ',')}}</span>
 				</span>
@@ -18,11 +18,9 @@
                                  data-sum="{{$dept_price['C']}}">{{number_format($dept_price['C'], 1, '.', ',')}}</span>
                         方包:$<span class="dept-total" data-dept="D"
                                   data-sum="{{$dept_price['D']}}">{{number_format($dept_price['D'], 1, '.', ',')}}</span>
-{{--                        樓:$<span class="dept-total" data-dept="F"--}}
-{{--                                 data-sum="{{$dept_price['F']}}">{{number_format($dept_price['F'], 1, '.', ',')}}</span>--}}
+                        造型包:$<span class="dept-total" data-dept="E"
+                                 data-sum="{{$dept_price['E']}}">{{number_format($dept_price['E'], 1, '.', ',')}}</span>
                     </div>
                 </td>
-                <td width="194px" align="center" bgcolor="#CCFFFF"></td>
-                <td width="200px" align="center" bgcolor="#CCFFFF"></td>
             </tr>
         </table>
