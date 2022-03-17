4ee143c6721106de79b0898576c67557cfed0a61
jianl <fs378354476@outlook.com>
2022-03-17 04:46:53 +0000

    logo banner 背景更換

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -3,7 +3,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
     <div class="container">
         <a class="navbar-brand col-4" href="/">
-            <img src="/images/ryoyu_logo.png" alt="" width="80%" height="80%">
+            <img src="/images/2cafe_logo.png" alt="" width="40%" height="40%">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
